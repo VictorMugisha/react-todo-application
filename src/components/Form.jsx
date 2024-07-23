5efe42860e78178c6cc54ab683e01c9b448dcd29
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-23 17:01:13 +0200

    Implemented form management with state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+  const [input, setInput] = useState({
+    todoTitle: '',
+    isComplete: false
+  })
+  function handleChange(event) {
+    const { value, name } = event.target
+    setInput(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value
+      }
+    })
+  }
   return (
     <form>
-        <input type="text" placeholder="Add your new todo" name="todo" />
-        <button><i class="uil uil-plus"></i></button>
+      <input type="text" placeholder="Add your new todo" name="todoTitle" value={input.todoTitle} onChange={handleChange} />
+      <button><i class="uil uil-plus"></i></button>
     </form>
   )
 }
